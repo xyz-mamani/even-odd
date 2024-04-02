512964d13e33cf24941dd275c05f6b8d37a9e52f
Mamani <mamani.developer@gmail.com>
2024-04-02 17:51:28 -0400

    Implement the function even or odd

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,9 @@
+import 'dart:io';
+
+import 'package:even_odd/even_odd.dart';
+
 void main() {
-  print('Hello, World!');
+  print('Enter a number');
+  var number = int.parse(stdin.readLineSync().toString());
+  print(evenOdd(number));
 }
